39311b8984acf65d5bc29c56382f9de31474d602
TRUONGNGUYEN <truongvan.nguyen@isobar.com>
2020-03-18 11:02:32 +0700

    Fix bug layout

--- js/theme.js
+++ js/theme.js
@@ -13,7 +13,40 @@ $(document).ready(function () {
             autoScrolling: false,
             controlArrows: false,
             slidesNavigation: true
+
+        });
+    }
+
+    if($('.service__choices').length) {
+        $('.service__choices').slick({
+            arrows: true,
+            autoplay: false,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 900,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        arrows: false,
+                        slidesToShow: 1,
+                    }
+                }
+            ]
         });
     }
 
 });
+
+var goToServiceStep = function(index) {
+    $('.service__form').hide();
+    $('.service__form:eq(' + index +')').stop().fadeIn("slow");
+};
