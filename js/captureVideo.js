5c89eeeac3166f6713bec44bdec35cfc9efa9d44
TruongNg <vantruong269@gmail.com>
2020-03-23 13:43:17 +0700

    Fix bug layout

--- js/captureVideo.js
+++ js/captureVideo.js
@@ -1,71 +1,85 @@
-function handleError(error) {
-    console.error('navigator.getUserMedia error: ', error);
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices &&
+        navigator.mediaDevices.getUserMedia);
 }
-const constraints = {video: true};
 
-(function() {
-    const screenshotButtonBack = document.querySelector('#captureBtn-back');
-    const confirmButtonBack = document.querySelector('#captureBtn-confirm-back');
-    const imgBack = document.querySelector('#screenshot-back');
-    const videoBack = document.querySelector('#video-back');
-
-    const screenshotButtonFront = document.querySelector('#captureBtn-front');
-    const confirmButtonFront = document.querySelector('#captureBtn-confirm-front');
-    const imgFront = document.querySelector('#screenshot-front');
-    const videoFront = document.querySelector('#video-front');
-
-    const canvasBack = document.createElement('canvas');
-    const canvasFront = document.createElement('canvas');
-
-    startVideo();
-
-    screenshotButtonBack.onclick = function() {
-        if (videoBack.paused){
-            confirmButtonBack.style.display = 'none';
-            screenshotButtonBack.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp hình';
-            startVideo();
-        }else {
-            canvasBack.width = videoBack.videoWidth;
-            canvasBack.height = videoBack.videoHeight;
-            canvasBack.getContext('2d').drawImage(videoBack, 0, 0);
-            // Other browsers will fall back to image/png
-            imgBack.src = canvasBack.toDataURL('image/webp');
-            stopVideo(videoBack, screenshotButtonBack, confirmButtonBack);
+if (hasGetUserMedia()) {
+    function handleError(error) {
+        console.error('navigator.getUserMedia error: ', error);
+    }
+    const constraints = {
+        video: {
+            width: 516,
+            height: 382
         }
     };
 
-    screenshotButtonFront.onclick = function() {
-        if (videoFront.paused){
-            confirmButtonFront.style.display = 'none';
-            screenshotButtonFront.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp hình';
-            startVideo();
-        }else {
-            canvasFront.width = videoFront.videoWidth;
-            canvasFront.height = videoFront.videoHeight;
-            canvasFront.getContext('2d').drawImage(videoFront, 0, 0);
-            // Other browsers will fall back to image/png
-            imgFront.src = canvasFront.toDataURL('image/webp');
-            stopVideo(videoFront, screenshotButtonFront, confirmButtonFront);
+    (function() {
+        const screenshotButtonBack = document.querySelector('#captureBtn-back');
+        const confirmButtonBack = document.querySelector('#captureBtn-confirm-back');
+        const imgBack = document.querySelector('#screenshot-back');
+        const videoBack = document.querySelector('#video-back');
+
+        const screenshotButtonFront = document.querySelector('#captureBtn-front');
+        const confirmButtonFront = document.querySelector('#captureBtn-confirm-front');
+        const imgFront = document.querySelector('#screenshot-front');
+        const videoFront = document.querySelector('#video-front');
+
+        const canvasBack = document.createElement('canvas');
+        const canvasFront = document.createElement('canvas');
+
+        startVideo();
+
+        screenshotButtonBack.onclick = function() {
+            if (videoBack.paused){
+                confirmButtonBack.style.display = 'none';
+                screenshotButtonBack.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp hình';
+                startVideo();
+            }else {
+                canvasBack.width = videoBack.videoWidth;
+                canvasBack.height = videoBack.videoHeight;
+                canvasBack.getContext('2d').drawImage(videoBack, 0, 0);
+                // Other browsers will fall back to image/png
+                imgBack.src = canvasBack.toDataURL('image/webp');
+                stopVideo(videoBack, screenshotButtonBack, confirmButtonBack);
+            }
+        };
+
+        screenshotButtonFront.onclick = function() {
+            if (videoFront.paused){
+                confirmButtonFront.style.display = 'none';
+                screenshotButtonFront.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp hình';
+                startVideo();
+            }else {
+                canvasFront.width = videoFront.videoWidth;
+                canvasFront.height = videoFront.videoHeight;
+                canvasFront.getContext('2d').drawImage(videoFront, 0, 0);
+                // Other browsers will fall back to image/png
+                imgFront.src = canvasFront.toDataURL('image/webp');
+                stopVideo(videoFront, screenshotButtonFront, confirmButtonFront);
+            }
+        };
+
+        let localMediaStream;
+
+        function handleSuccess(stream) {
+            localMediaStream = stream;
+            videoBack.srcObject = stream;
+            videoFront.srcObject = stream;
         }
-    };
 
-    let localMediaStream;
+        function startVideo() {
+            navigator.mediaDevices.getUserMedia(constraints).
+            then(handleSuccess).catch(handleError);
+        }
 
-    function handleSuccess(stream) {
-        localMediaStream = stream;
-        videoBack.srcObject = stream;
-        videoFront.srcObject = stream;
-    }
+        function stopVideo(video, screenshotBtn, confirmBtn) {
+            video.pause();
+            screenshotBtn.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp lại';
+            confirmBtn.style.display = 'inline-block';
+        }
 
-    function startVideo() {
-        navigator.mediaDevices.getUserMedia(constraints).
-        then(handleSuccess).catch(handleError);
-    }
-
-    function stopVideo(video, screenshotBtn, confirmBtn) {
-        video.pause();
-        screenshotBtn.innerHTML = '<i class="fa fa-camera"></i>&nbsp;Chụp lại';
-        confirmBtn.style.display = 'inline-block';
-    }
-
-})();
+    })();
+} else {
+    alert('getUserMedia() is not supported by your browser');
+}
